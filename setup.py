d70f9746ce60bb810a96a3f20687e82f122c8f64
PierXuY <wxuymail@163.com>
2023-09-17 20:36:47 +0800

    modify setup.py

--- setup.py
+++ setup.py
@@ -8,12 +8,11 @@ with codecs.open(os.path.join(here, "README.md"), encoding="utf-8") as fh:
     long_description = fh.read()
 
 setup(
-    name="html_to_dash",
-    version='0.1.8',
+    name="html-to-dash",
+    version='0.2.0',
     author="PierXuY",
-    author_email="wxuymail@163.com",
     description="Convert HTML to dash format.",
-    url="https://github.com/PierXuY/html_to_dash",
+    url="https://github.com/PierXuY/html-to-dash",
     long_description_content_type="text/markdown",
     long_description=long_description,
     packages=find_packages(),
@@ -21,9 +20,6 @@ setup(
     keywords=['html', 'dash'],
     python_requires=">=3.6",
     classifiers=[
-        "Development Status :: 1 - Planning",
-        "Intended Audience :: Developers",
         "Programming Language :: Python :: 3",
-        'Operating System :: OS Independent'
     ]
 )
